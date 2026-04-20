a4124fe171e564181c8a7e1980fdfe4e17e87e0f
Himel Islam <himel7100@gmail.com>
2026-04-20 13:20:50 +0600

    fix: added vercel.json

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import path from 'path'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// })
+
+
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  // server: {
+  //   port: 4000,
+  // },
+  // define: {
+  //   'import.meta.env.VITE_API_URL': JSON.stringify('http://localhost:8000'),
+  // },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
